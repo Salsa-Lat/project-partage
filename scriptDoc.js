3b7078ecae4261c07d838a1b0d2e503efa69040a
Mehdi <elmehdi.ettsouli@latoile.em-lyon.com>
2026-02-02 12:25:22 +0100

    push

---
+++ scriptDoc.js
@@ -0,0 +1,6 @@
+const showImageButton = document.getElementById("btn-userflow");
+const imgUserflow = document.getElementById("img-userflow");
+
+showImageButton.addEventListener("click", () => { 
+         imgUserflow.classList.toggle("visible"); 
+      });
